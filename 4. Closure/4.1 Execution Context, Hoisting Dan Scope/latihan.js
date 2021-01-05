f4d1bd2fc5536f6f3ec9baa713c860785fe91399
unknown <aditnegarabisnis@gmail.com>
2021-01-05 20:09:05 +0700

    Learn : Execution Context, Hoisting & Scope

---
+++ 4. Closure/4.1 Execution Context, Hoisting Dan Scope/latihan.js
@@ -0,0 +1,21 @@
+var name = 'Aditya';
+var age = '22';
+console.log(sayHello());
+function sayHello(){
+    return `Hello my name is ${name}, im ${age} years old`;; 
+}
+
+function satu(){
+    var nama = 'Aditya';
+    console.log(nama);
+}
+
+function dua(){
+    console.log(nama);
+}
+
+console.log(nama);
+var nama = 'Ayesha';
+satu();
+dua('Aura')
+console.log(nama); 
---
+++ 4. Closure/4.1 Execution Context, Hoisting Dan Scope/script.js
@@ -0,0 +1,17 @@
+
+
+
+// Creation phase pada Global Context
+    // 1. Javascript mencari variable atau function pada file
+    // Jika ada variable maka akan diision
+    //  nama var = undefined
+    //  Jika ada function maka diisikan
+    // nama function - fn()
+    // Konsep ini disebut hoisting
+    // javascript mendefinisikan window = global object 
+    // javascript mendefinisikan this = window
+
+// Execution Phase
+    // Mengeksekusi program baris/baris dari atas ke bawah
+
+
